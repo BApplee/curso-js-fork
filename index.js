5f738e24a80c2bc23db01368bcf4a535d5ecbe7a
Axel Choque <axel_chooque@hotmail.com>
2022-07-26 18:07:26 -0300

    YARD Sale clase 20 terminada

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const mailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-mailMenu.addEventListener('click',toogleDesktopMenu);
+const menuIconMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
-function toogleDesktopMenu()
+mailMenu.addEventListener('click',toggleDesktopMenu);
+menuIconMobile.addEventListener('click',toggleMobileMenu);
+cartIcon.addEventListener('click',toggleCartMenu)
+
+function toggleDesktopMenu()
 {
     desktopMenu.classList.toggle('inactive');
+    if (!productDetail.classList.toggle('inactive'))
+    {
+        productDetail.classList.add('inactive');
+    }
+}
+
+function toggleMobileMenu()
+{
+    mobileMenu.classList.toggle('inactive');
+    if (!productDetail.classList.toggle('inactive'))
+    {
+        productDetail.classList.add('inactive');
+    }
+}
+
+function toggleCartMenu(){
+    productDetail.classList.toggle('inactive');
+
+    if (!mobileMenu.classList.contains('inactive'))
+    {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!desktopMenu.classList.contains('inactive'))
+    {
+        desktopMenu.classList.add('inactive');
+    }
 }
